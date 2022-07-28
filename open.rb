7b6f331756be4e20f4470a3c6f36373ff5564026
Grimmgork <mmmru7shj@gmail.com>
2022-07-28 17:29:39 +0100

    adad

--- open.rb
+++ open.rb
@@ -3,4 +3,6 @@ require_relative './app.rb'
 url = ARGV[0] || "gopher://gopher.floodgap.com"
 gurl = GopherUrl.new(url)
 
-`start http://localhost:5678/#{Application.GetProxyPath(gurl).gsub(" ", "+").gsub("&", "%26").gsub("%5C").gsub("\"", "%22")}`
+puts Application.GetProxyPath(gurl)
+
+`start http://localhost:5678#{Application.GetProxyPath(gurl)}`
