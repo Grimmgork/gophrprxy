d5805c44ca298a508c8818d33c9ea46beb014c5d
Eric <mmmru7shj@gmail.com>
2022-09-28 20:04:24 +0200

    + open-url.rb

---
+++ open-url.rb
@@ -0,0 +1,10 @@
+require 'cgi'
+
+port = ARGV[1].to_i
+host = ARGV[0]
+if host =~ /[ *~#"']/
+	throw "Invalid Hostname!"
+end
+url = ARGV[2].strip
+
+`start http://#{host}:#{port}/url?#{CGI.escape(url)}`
